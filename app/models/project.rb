7d4abddf138a8e50f37165dba1852b24f29384c0
Forrest Scofield <fsdcofield@gmail.com>
2021-12-02 21:17:46 -0500

    pull from JSON file

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,17 @@
-class Project < ApplicationRecord
+class Project
+  include ActiveModel::Model
+  attr_accessor :name, :ticker, :website, :audit, :icon_url, :ticker_url, :contract_url
+
+  class << self
+    def all
+      @all ||= project_data.map do |project_json|
+        new(project_json)
+      end
+    end
+
+    def project_data
+        file_name = Rails.root + "config/projects.json"
+        JSON.parse(File.read(file_name), symbolize_names: true)[:projects].freeze if File.exist?(file_name)
+    end
+  end
 end
